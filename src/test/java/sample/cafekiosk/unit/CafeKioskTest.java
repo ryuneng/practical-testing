9090615ff6e61f92603cbaa3fd32a6054ca48324
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-08-04 22:59:22 +0900

    섹션2 - 단위 테스트
    1. 수동 테스트, 자동화 테스트
     - 수동 테스트 : 최종적으로 사람의 검증 절차 필요
     - 자동화 테스트 : 사람의 검증 절차 불필요
    
    2. JUnit5, AssertJ
     - JUnit5: 단위 테스트를 위한 프레임워크
     - AssertJ로 보다 명시적인 테스트 코드 작성 가능, 풍부한 메서드 지원
    
    3. 해피 케이스, 예외 케이스
    4. 경계값 테스트
     ex) 조건: 10~22시
     - 해피케이스: 10시, 예외케이스: 9시59분
    
    5. 테스트하기 쉬운/어려운 영역 구분
     - 테스트하기 어려운 영역은 외부로 분리하기

---
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -0,0 +1,126 @@
+package sample.cafekiosk.unit;
+
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CafeKioskTest {
+
+    @Test
+    void add_manual_test() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+//        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        // 경계값 테스트 - 해피케이스 (기대한 범위의 숫자)
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        // 경계값 테스트 - 예외케이스 (0잔은 주문 불가)
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    // 1. CafeKiosk.java 에서 currentDateTime을 LocalDateTime.now();로 설정하는 경우에는
+    // 실제 테스트를 실행하는 시간이 SHOP_OPEN_TIME 혹은 SHOP_CLOSE_TIME을 벗어난다면,
+    // 테스트 결과가 무조건 실패로 나오기 때문에 해당 테스트는 적절한 테스트가 아님
+//    @Test
+//    void createOrder() {
+//        CafeKiosk cafeKiosk = new CafeKiosk();
+//        Americano americano = new Americano();
+//        cafeKiosk.add(americano);
+//
+//        Order order = cafeKiosk.createOrder();
+//
+//        assertThat(order.getBeverages()).hasSize(1);
+//        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+//    }
+
+    // 2-1. 테스트하기 어려운 영역을 외부로 분리하기 - LocalDateTime을 파라미터로 외부에서 받도록 수정하여 적절한 테스트 가능
+    @Test
+    void createOrderWithCurrentTime() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        // 해피케이스 - 가게 오픈 시간이 10시이기 때문에 적절한 경계값 테스트를 위해 10시로 지정
+        Order order = cafeKiosk.createOrder(LocalDateTime.of(2024, 8, 4, 10, 0));
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    // 2-2.
+    @Test
+    void createOrderOutsideOpenTime() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        // 예외케이스 - 오픈 시간 10시에서 1분 뺀 경계값 테스트
+        assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2024, 8, 4, 9, 59)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+    }
+
+}
