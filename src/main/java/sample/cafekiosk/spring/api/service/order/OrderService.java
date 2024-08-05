a67e4aac128930b855a8d2c9b114bbe0f052e0b0
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-08-05 09:32:04 +0900

    섹션5 - Business Layer 테스트 (2)
    - 중복되는 상품번호 리스트로 주문을 생성하는 테스트 코드, 비즈니스 로직 코드 작성
    
    1. @AfterEach tearDown()
     - 전체 테스트 진행 시, 데이터 클렌징 작업을 수행해야 개별 테스트 간에 영향을 주지 않는다.
     - @DataJpaTest는 @Transactional을 가지고 있어 자동으로 롤백이 되고, @SpringBootTest는 @Transactional이 없어서 자동으로 롤백이 되지 않는다.
    
    2. REST API (.http 파일)
     - .http 파일을 생성하면 인텔리제이에서 REST API를 빠르고 쉽게 쓸 수 있도록 지원해준다.
     - 파일 내에서는 ### 으로 API 요청의 제목을 지정할 수 있다.

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,11 +23,44 @@ public class OrderService {
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+
+        // ### RED 테스트 프로세스
+        // 1. 주어진 상품 번호 리스트에 해당하는 모든 상품을 데이터베이스에서 조회한다.
+        //  * In절은 중복 데이터를 필터링하기 때문에 productNumbers 리스트에 중복되는 상품번호가 있더라도 결과는 중복이 제거된 products가 조회된다.
+        //    따라서, 아래의 products만으로는 중복되는 상품번호 리스트로 주문 생성이 불가하기 때문에 Product 객체를 가공하는 로직을 추가해야 한다.
+//        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+
+        // ### GREEN 테스트 프로세스 (2~3 추가)
+        // 2. 조회한 상품들을 Map으로 변환한다.
+        //    productNumber를 기반으로 Product 객체를 빨리 찾을 수 있게 한다.
+//        Map<String, Product> productMap = products.stream()
+//                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        // 3. 원래의 상품 번호 리스트를 순회하면서 productMap에서 해당 상품 번호에 대한 Product 객체를 가져와 리스트로 만든다.
+//        List<Product> duplicateProducts = productNumbers.stream()
+//                .map(productMap::get)
+//                .collect(Collectors.toList());
+
+        // ### REFACTOR 프로세스 1
+        List<Product> products = findProductsBy(productNumbers);
 
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder);
+    }
+
+    // ### REFACTOR 프로세스 2
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        // createOrder의 1번 로직
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        // createOrder의 2번 로직
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        // createOrder의 3번 로직
+        return productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
@@ -28,7 +31,26 @@ class OrderServiceTest {
     private ProductRepository productRepository;
 
     @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
     private OrderService orderService;
+
+    // 1. 전체 테스트를 진행했을 때, createOrderWithDuplicateProductNumbers 테스트가 createOrder 테스트에 영향을 미쳐 createOrder 테스트는 실패하게 된다.
+    //   -> Map을 만들면서 키에 대한 (productNumber 001 데이터) 중복이 발생한다.
+    // 2. 따라서, @AfterEach문을 통해 데이터 클렌징 작업을 수행해야 한다.
+    // 3. ProductRepositoryTest는 데이터 클렌징 작업이 없어도 전체 테스트가 성공하는데, 이유는?
+    //   -> @SpringBootTest와 @DataJpaTest 차이에 있다.
+    //      DataJpaTest는 @Transactional을 가지고 있어 자동으로 롤백이 되고, SpringBootTest는 @Transactional이 없어서 자동으로 롤백이 되지 않는다.
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -41,7 +63,7 @@ class OrderServiceTest {
         Product product2 = createProduct(HANDMADE, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
         productRepository.saveAll(List.of(product1, product2, product3));
-        // 2. 주문 생성 시 요청할 OrderCreateRequest 객체에 상품번호 리스트 필드 값을 설정해둔다.
+        // 2. 주문 생성 시 요청할 OrderCreateRequest 객체에 주문할 상품번호 리스트 필드 값을 설정해둔다.
         OrderCreateRequest request = OrderCreateRequest.builder()
                 .productNumbers(List.of("001", "002"))
                 .build();
@@ -64,6 +86,41 @@ class OrderServiceTest {
                         tuple("002", 3000)
                 );
 
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        // given
+        // 1. 상품을 생성한다.
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+        // 2. 주문 생성 시 요청할 OrderCreateRequest 객체에 주문할 상품번호 리스트 필드 값을 중복되는 번호로 설정해둔다.
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001")) // 중복되는 상품번호
+                .build();
+
+        // when - 주문을 생성한다.
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        // 1. 생성된 주문의 아이디가 NotNull인지 체크한다.
+        assertThat(orderResponse.getId()).isNotNull();
+        // 2. 주문생성시간과 총주문금액의 값을 체크한다.
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2000);
+        // 3. 주문된 상품 개수를 체크하고, 상품번호와 상품가격의 값을 체크한다.
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000)
+                );
     }
     
     private Product createProduct(ProductType type, String productNumber, int price) {
