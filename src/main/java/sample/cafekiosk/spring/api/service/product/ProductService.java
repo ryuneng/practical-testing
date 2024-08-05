af3635899cf0c87f269b5210ba618e2c746e31c9
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-08-05 14:04:02 +0900

    섹션5 - Presentation Layer 테스트 (1)
    - 신규 상품 등록 테스트 코드, 비즈니스 로직 코드 작성
    
    1. Presentation Layer 테스트 (Controller 테스트)
     - Business Layer와 Persistence Layer는 Mocking 처리하고, 단위 테스트에 가까운 테스트를 진행한다.
    
    2. readOnly = true 읽기전용
     - CQRS(Command Query Responsibility Segregation)
      : Command(CUD) 서비스와 Query(Read) 서비스의 책임을 분리해서 관리하는 아키텍처 패턴

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -10,19 +12,49 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기전용
+ * CRUD 에서 CUD 동작 X / Only Read
+ * JPA : CUD 스냅샷 저장, 변경 감지 X -> 성능 향상
+ * 
+ * CQRS - Command(CUD) / Read 분리
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    // 동시성 이슈
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
         return products.stream()
-                .map(product -> ProductResponse.of(product))
+                .map(ProductResponse::of)
                 .collect(Collectors.toList());
-
     }
 
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009 / 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
+    }
 }
